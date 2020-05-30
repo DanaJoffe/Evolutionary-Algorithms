a63f189eb5c1b74e3dc183494fecb5f84052b88f
DanaJoffe <djoffe94@gmail.com>
2020-05-30 21:05:11 +0300

    change relationship between Component & Node, add to Constant a range option, add string representation to Components

--- ex2/GeneticProgrammingAPI/Component.py
+++ ex2/GeneticProgrammingAPI/Component.py
@@ -1,42 +1,142 @@
+import math
+import random
+from abc import ABC, abstractmethod
+from inspect import signature
+from typing import Optional, Tuple
+
+""" basics """
 
 
-class Node(object):
-    """ tree node """
-    def __init__(self):
-        self.sons = []  # [Node(0) for _ in range(sons_num)]
-
-    def add_son(self, son: "Node"):
-        self.sons.append(son)
-
-
-class Component(Node):
-    def __init__(self, arity):
+class Component(ABC):
+    def __init__(self, arity: int, symbol: str = '', string_repr=None):
         super().__init__()
         self.arity = arity
-        self.inputs = 0  # should be arity's size
+        self.symbol = symbol
+        self.string_repr = string_repr
+
+    def __repr__(self):
+        return self.symbol
+
+    @abstractmethod
+    def calc(self, values, *args, **kwargs):
+        raise NotImplemented
+
+    def __str__(self):
+        if self.string_repr:
+            return self.string_repr
+        return self.symbol
+
+    def __call__(self, *args, **kwargs) -> "Component":
+        """ create this component"""
+        return self._create()
+
+    def _create(self) -> "Component":
+        return self.__copy__()
+
+    @abstractmethod
+    def __copy__(self) -> "Component":
+        raise NotImplemented
 
 
 class Variable(Component):
-    def __init__(self, name: str):
-        super().__init__(arity=0)
+    def __init__(self, name: str, *args, **kwargs):
+        super().__init__(*args, arity=0, symbol=name, **kwargs)
         self.name = name
 
+    def calc(self, values, *args, **kwargs):
+        value = kwargs.get(self.name, None)
+        assert value is not None, f"missing variable {self.name}"
+        return value
 
-class Operator(Component):
-    def __init__(self, op_func, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.op_func = op_func
+    def __copy__(self):
+        class_type = self.__class__
+        instance = class_type(self.name)
+        return instance
+        # return Variable(self.name)
 
 
 class Constant(Component):
-    def __init__(self, value):
-        super().__init__(arity=0)
-        self.value = value
+    def __init__(self, value=None, range: Tuple[int, int]=None, integer=False, **kwargs):
+        self._value = value
+        self._range = range
+        self._integer = integer
+        if value:
+            self.value = value
+        elif integer:
+            # raise Exception("Integer Constant - Not Supported")
+            self.value = random.randint(*range)
+        else:
+            self.value = random.uniform(*range)
+        super().__init__(arity=0, symbol=str(self.value), *kwargs)
+
+    def calc(self, *args, **kwargs):
+        return self.value
+
+    def __copy__(self):
+        class_type = self.__class__
+        instance = class_type(self.value)
+        return instance
+        # return Constant(self.value)
+
+    def _create(self) -> "Component":
+        return Constant(self._value, self._range, self._integer)
 
 
-class Condition(Component):
-    def __init__(self, cond_func, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cond_func = cond_func
+class Function(Component):
+    def __init__(self, func, *args, **kwargs):
+        params = signature(func).parameters
+        super().__init__(*args, arity=len(params), **kwargs)
+        self.func = func
+
+    def calc(self, values, *args, **kwargs):
+        return self.func(*values)
+
+    def __copy__(self):
+        class_type = self.__class__
+        instance = class_type(self.func, symbol=self.symbol, string_repr=self.string_repr)
+        return instance
+
+
+""" name typedef """
+
+
+class Operator(Function):
+    pass
+
+
+class Condition(Function):
+    pass
+
+
+""" support string representation """
+
+
+class BinaryOperator(Operator):
+    def __str__(self):
+        ret = '({}' + self.symbol + '{})'
+        return ret
+
+
+class PrefixUnaryOperator(Operator):
+    def __str__(self):
+        ret = '(' + self.symbol + '{})'
+        return ret
+
+
+class PostfixUnaryOperator(Operator):
+    def __str__(self):
+        ret = '({}' + self.symbol + ')'
+        return ret
+
+
+PLUS     = BinaryOperator(lambda x, y: x + y, symbol='+')
+SUBTRACT = BinaryOperator(lambda x, y: x - y, symbol='-')
+MULTIPLY = BinaryOperator(lambda x, y: x * y, symbol='*')
+DIVIDE   = BinaryOperator(lambda x, y: x / y if y != 0 else math.inf, symbol='/')
+
+MINUS   = PrefixUnaryOperator(lambda x: -x, symbol='-')
+SQUARED = PostfixUnaryOperator(lambda x: x**2, symbol='^2')
+
+IF_ELSE = Condition(lambda x,y,z: y if x else z, symbol='if else', string_repr='(if {} then {} else {})')
 
 
---
+++ ex2/GeneticProgrammingAPI/Node.py
@@ -0,0 +1,136 @@
+from abc import abstractmethod
+from typing import Optional, List
+
+from GeneticProgrammingAPI.Component import Component
+
+
+class Node(object):
+    """ tree node """
+    def __init__(self, content):
+        self._content: Component = content
+
+        self.children: List[Node] = []  # [Node(0) for _ in range(sons_num)]
+        self._nodes = 1  # subtree nodes (including self)
+        self._depth = 0  # subtree depth
+        self.parent: Optional[Node] = None
+        self._level = 0  # relative to head
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def level(self) -> int:
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        """ rolling setter: rolling down the tree """
+        self._level = value
+        for child in self.children:
+            child.level = self._level + 1
+
+    @property
+    def depth(self) -> int:
+        return self._depth
+
+    @depth.setter
+    def depth(self, value):
+        """ rolling setter: rolling up the tree """
+        self._depth = value
+        if self.parent:
+            self.parent.depth = max(child.depth for child in self.parent.children) + 1
+
+    @property
+    def nodes(self) -> int:
+        return self._nodes
+
+    @nodes.setter
+    def nodes(self, value):
+        """ rolling setter: rolling up the tree """
+        self._nodes = value
+        if self.parent:
+            self.parent.nodes = sum(child.nodes for child in self.parent.children) + 1
+
+    def _set_parent(self, parent: "Node"):
+        self.parent = parent
+
+    def add_child(self, child: "Node"):
+        self.children.append(child)
+        child._set_parent(self)
+        return self
+
+    def replace_node(self, other: "Node"):
+        assert self.parent is not None, "must have parent"
+        self.parent._replace_child(self, other)
+
+    def _replace_child(self, child: "Node", other: "Node"):
+        """ replace self's child-subtree with other subtree """
+        child.parent = None
+        other.parent = self
+        # todo: improve
+        for i, node in enumerate(self.children):
+            if node == child:
+                self.children[i] = other
+                break
+
+        # update nodes amount & subtree depth in parent (self)
+        other.nodes = other.nodes  # updates parent
+        other.depth = other.depth  # updates parent
+        # update level in new child subtree (other)
+        other.level = self.level + 1
+        # update level in old child subtree (child)
+        child.level = 0
+
+    def set_content(self, content):
+        self._content = content
+
+    def __copy__(self) -> "Node":
+        copy: Node = Node(self._content.__copy__())
+        for child in self.children:
+            # set children and parents
+            copy.add_child(child.__copy__())
+        copy._nodes = self._nodes
+        copy._depth = self._depth
+        copy._level = self._level
+        return copy
+
+    # Component
+
+    @property
+    def arity(self):
+        return self._content.arity
+
+    @property
+    def symbol(self):
+        return self._content.symbol
+
+    def __repr__(self):
+        return self._content.__repr__()
+
+    def calc(self, *args, **kwargs):
+        # recursive call
+        values = [child.calc(*args, **kwargs) for child in self.children]
+        assert len(values) == self.arity, f"children amount is wrong"
+        return self._content.calc(values, *args, **kwargs)
+
+    def __str__(self):
+        # recursive call
+        values = [child.__str__() for child in self.children]
+        assert len(values) == self.arity, f"children amount is wrong"
+        ret = self._content.__str__().format(*values)
+        return ret
+
+    def __call__(self, *args, **kwargs) -> "Component":
+        """ create this component"""
+        return self._content.__call__()
+
+    def get_nodes(self):
+        return [node for node in iterate_tree(self)]
+
+
+def iterate_tree(head: Node):
+    yield head
+    for child in head.children:
+        yield from iterate_tree(child)
+
